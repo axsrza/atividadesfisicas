e11a8d542c7d89eee6672610355fa012d16ffc87
axsrza <102383441+axsrza@users.noreply.github.com>
2023-12-26 20:56:33 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 // Configuração do Firebase
 const firebaseConfig = {
-    projectId: "atividadesfisicas-e9c51",  // Use o ID do projeto Firebase
+    apiKey: "SuaChaveDeAPI",
+    authDomain: "atividadesfisicas-e9c51.firebaseapp.com",
+    projectId: "atividadesfisicas-e9c51",
+    storageBucket: "atividadesfisicas-e9c51.appspot.com",
+    messagingSenderId: "SeuID",
+    appId: "SuaAppID"
 };
 
 // Inicialização do Firebase
@@ -8,6 +13,51 @@ firebase.initializeApp(firebaseConfig);
 
 // Inicialização do Firestore
 const db = firebase.firestore();
+
+// Lógica para exibir os campos de detalhes conforme o tipo de atividade selecionada
+document.getElementById("tipoAtividade").addEventListener("change", function () {
+    const tipoAtividade = this.value;
+
+    document.getElementById("detalhesCaminhada").style.display = tipoAtividade === "caminhada" ? "block" : "none";
+    document.getElementById("detalhesPedalada").style.display = tipoAtividade === "pedalada" ? "block" : "none";
+    document.getElementById("detalhesPassos").style.display = tipoAtividade === "passos" ? "block" : "none";
+});
+
+// Lógica para adicionar atividade
+function adicionarAtividade() {
+    const tipoAtividade = document.getElementById("tipoAtividade").value;
+    const data = new Date();
+
+    let detalhes;
+    if (tipoAtividade === "caminhada") {
+        const km = parseFloat(document.getElementById("km").value);
+        const tempo = parseInt(document.getElementById("tempo").value);
+        detalhes = { km, tempo };
+    } else if (tipoAtividade === "pedalada") {
+        const kmPedalada = parseFloat(document.getElementById("kmPedalada").value);
+        const tempoPedalada = parseInt(document.getElementById("tempoPedalada").value);
+        detalhes = { kmPedalada, tempoPedalada };
+    } else if (tipoAtividade === "passos") {
+        const passos = parseInt(document.getElementById("passos").value);
+        detalhes = { passos };
+    }
+
+    // Adicionar atividade ao Firestore
+    db.collection("atividades").add({
+        tipo: tipoAtividade,
+        detalhes: detalhes,
+        data: data
+    })
+        .then(function (docRef) {
+            console.log("Atividade adicionada com ID: ", docRef.id);
+        })
+        .catch(function (error) {
+            console.error("Erro ao adicionar atividade: ", error);
+        });
+
+    // Limpar campos após adicionar atividade (opcional)
+    limparCampos();
+}
 
 // Função para limpar campos do formulário (opcional)
 function limparCampos() {
@@ -59,26 +109,42 @@ function exibirGraficos() {
     });
 }
 
-// Lógica para exibir os campos de detalhes conforme o tipo de atividade selecionada
-document.getElementById("tipoAtividade").addEventListener("change", function() {
-    const tipoAtividade = this.value;
+// Função para criar gráfico usando Chart.js
+function criarGrafico(idCanvas, label, dados) {
+    const ctx = document.getElementById(idCanvas).getContext('2d');
+    new Chart(ctx, {
+        type: 'line',
+        data: {
+            datasets: [{
+                label: label,
+                data: dados,
+                borderColor: 'rgba(75, 192, 192, 1)',
+                borderWidth: 1,
+                fill: false
+            }]
+        },
+        options: {
+            scales: {
+                x: {
+                    type: 'time',
+                    time: {
+                        unit: 'day'
+                    },
+                    title: {
+                        display: true,
+                        text: 'Data'
+                    }
+                },
+                y: {
+                    title: {
+                        display: true,
+                        text: label === 'Passos' ? 'Quantidade de Passos' : 'Distância (KM)'
+                    }
+                }
+            }
+        }
+    });
+}
 
-    document.getElementById("detalhesCaminhada").style.display = tipoAtividade === "caminhada" ? "block" : "none";
-    document.getElementById("detalhesPedalada").style.display = tipoAtividade === "pedalada" ? "block" : "none";
-    document.getElementById("detalhesPassos").style.display = tipoAtividade === "passos" ? "block" : "none";
-});
-
-// Lógica para adicionar atividade
-function adicionarAtividade() {
-    const tipoAtividade = document.getElementById("tipoAtividade").value;
-    const data = new Date();
-    
-    let detalhes;
-    if (tipoAtividade === "caminhada") {
-        const km = parseFloat(document.getElementById("km").value);
-        const tempo = parseInt(document.getElementById("tempo").value);
-        detalhes = { km, tempo };
-    } else if (tipoAtividade === "pedalada") {
-        const kmPedalada = parseFloat(document.getElementById("kmPedalada").value);
-        const tempoPedalada = parseInt(document.getElementById("tempoPedalada").value);
-        detalhes = { k
+// Chamar a função para exibir os gráficos
+exibirGraficos();
