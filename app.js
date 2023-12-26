6815b8d155ad97e92899ad6a539843ecd8f7ffb6
axsrza <102383441+axsrza@users.noreply.github.com>
2023-12-26 20:31:48 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,6 @@
 // Configuração do Firebase
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+    // Sua configuração do Firebase aqui
 };
 
 firebase.initializeApp(firebaseConfig);
@@ -14,12 +9,38 @@ firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 
 // Adicione aqui o código JavaScript para manipular os dados e interações com a página
-// Lembre-se de incluir lógica para adicionar, recuperar e exibir dados no Firestore
 
-// Exemplo básico para adicionar dados
-function adicionarAtividade(tipo, detalhes, data) {
+// Lógica para exibir os campos de detalhes conforme o tipo de atividade selecionada
+document.getElementById("tipoAtividade").addEventListener("change", function() {
+    const tipoAtividade = this.value;
+
+    document.getElementById("detalhesCaminhada").style.display = tipoAtividade === "caminhada" ? "block" : "none";
+    document.getElementById("detalhesPedalada").style.display = tipoAtividade === "pedalada" ? "block" : "none";
+    document.getElementById("detalhesPassos").style.display = tipoAtividade === "passos" ? "block" : "none";
+});
+
+// Lógica para adicionar atividade
+function adicionarAtividade() {
+    const tipoAtividade = document.getElementById("tipoAtividade").value;
+    const data = new Date();
+    
+    let detalhes;
+    if (tipoAtividade === "caminhada") {
+        const km = parseFloat(document.getElementById("km").value);
+        const tempo = parseInt(document.getElementById("tempo").value);
+        detalhes = { km, tempo };
+    } else if (tipoAtividade === "pedalada") {
+        const kmPedalada = parseFloat(document.getElementById("kmPedalada").value);
+        const tempoPedalada = parseInt(document.getElementById("tempoPedalada").value);
+        detalhes = { kmPedalada, tempoPedalada };
+    } else if (tipoAtividade === "passos") {
+        const passos = parseInt(document.getElementById("passos").value);
+        detalhes = { passos };
+    }
+
+    // Adicionar atividade ao Firestore
     db.collection("atividades").add({
-        tipo: tipo,
+        tipo: tipoAtividade,
         detalhes: detalhes,
         data: data
     })
@@ -29,4 +50,16 @@ function adicionarAtividade(tipo, detalhes, data) {
     .catch(function(error) {
         console.error("Erro ao adicionar atividade: ", error);
     });
+
+    // Limpar campos após adicionar atividade (opcional)
+    limparCampos();
+}
+
+// Função para limpar campos do formulário (opcional)
+function limparCampos() {
+    document.getElementById("km").value = "";
+    document.getElementById("tempo").value = "";
+    document.getElementById("kmPedalada").value = "";
+    document.getElementById("tempoPedalada").value = "";
+    document.getElementById("passos").value = "";
 }
