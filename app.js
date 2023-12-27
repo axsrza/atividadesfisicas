b86d8bf430da9f2f72d295a4061edc180771e6c7
axsrza <102383441+axsrza@users.noreply.github.com>
2023-12-26 22:40:44 -0300

    app.js

--- app.js
+++ app.js
@@ -44,62 +44,61 @@ const ChartTime = {
     },
 };
 
-document.addEventListener("DOMContentLoaded", function () {
-    // Lógica para exibir os campos de detalhes conforme o tipo de atividade selecionada
-    document.getElementById("tipoAtividade").addEventListener("change", function () {
-        const tipoAtividade = this.value;
+// Lógica para exibir os campos de detalhes conforme o tipo de atividade selecionada
+document.getElementById("tipoAtividade").addEventListener("change", function () {
+    const tipoAtividade = this.value;
 
-        document.getElementById("detalhesCaminhada").style.display = tipoAtividade === "caminhada" ? "block" : "none";
-        document.getElementById("detalhesPedalada").style.display = tipoAtividade === "pedalada" ? "block" : "none";
-        document.getElementById("detalhesPassos").style.display = tipoAtividade === "passos" ? "block" : "none";
-    });
+    document.getElementById("detalhesCaminhada").style.display = tipoAtividade === "caminhada" ? "block" : "none";
+    document.getElementById("detalhesPedalada").style.display = tipoAtividade === "pedalada" ? "block" : "none";
+    document.getElementById("detalhesPassos").style.display = tipoAtividade === "passos" ? "block" : "none";
+});
 
-    // Lógica para adicionar atividade
-    function adicionarAtividade() {
-        const tipoAtividade = document.getElementById("tipoAtividade").value;
-        const data = new Date();
+// Lógica para adicionar atividade
+function adicionarAtividade() {
+    const tipoAtividade = document.getElementById("tipoAtividade").value;
+    const data = new Date();
 
-        let detalhes;
-        if (tipoAtividade === "caminhada") {
-            const km = parseFloat(document.getElementById("km").value);
-            const tempo = parseInt(document.getElementById("tempo").value);
-            detalhes = { km, tempo };
-        } else if (tipoAtividade === "pedalada") {
-            const kmPedalada = parseFloat(document.getElementById("kmPedalada").value);
-            const tempoPedalada = parseInt(document.getElementById("tempoPedalada").value);
-            detalhes = { kmPedalada, tempoPedalada };
-        } else if (tipoAtividade === "passos") {
-            const passos = parseInt(document.getElementById("passos").value);
-            detalhes = { passos };
-        }
+    let detalhes;
+    if (tipoAtividade === "caminhada") {
+        const km = parseFloat(document.getElementById("km").value);
+        const tempo = parseInt(document.getElementById("tempo").value);
+        detalhes = { km, tempo };
+    } else if (tipoAtividade === "pedalada") {
+        const kmPedalada = parseFloat(document.getElementById("kmPedalada").value);
+        const tempoPedalada = parseInt(document.getElementById("tempoPedalada").value);
+        detalhes = { kmPedalada, tempoPedalada };
+    } else if (tipoAtividade === "passos") {
+        const passos = parseInt(document.getElementById("passos").value);
+        detalhes = { passos };
+    }
 
-        // Adicionar atividade ao Firestore
-        db.collection("atividades").add({
-            tipo: tipoAtividade,
-            detalhes: detalhes,
-            data: data
+    // Adicionar atividade ao Firestore
+    db.collection("atividades").add({
+        tipo: tipoAtividade,
+        detalhes: detalhes,
+        data: data
+    })
+        .then(function (docRef) {
+            console.log("Atividade adicionada com ID: ", docRef.id);
         })
-            .then(function (docRef) {
-                console.log("Atividade adicionada com ID: ", docRef.id);
-            })
-            .catch(function (error) {
-                console.error("Erro ao adicionar atividade: ", error);
-            });
+        .catch(function (error) {
+            console.error("Erro ao adicionar atividade: ", error);
+        });
 
-        // Limpar campos após adicionar atividade (opcional)
-        limparCampos();
-    }
+    // Limpar campos após adicionar atividade (opcional)
+    limparCampos();
+}
 
-    // Função para limpar campos do formulário (opcional)
-    function limparCampos() {
-        document.getElementById("km").value = "";
-        document.getElementById("tempo").value = "";
-        document.getElementById("kmPedalada").value = "";
-        document.getElementById("tempoPedalada").value = "";
-        document.getElementById("passos").value = "";
-    }
+// Função para limpar campos do formulário (opcional)
+function limparCampos() {
+    document.getElementById("km").value = "";
+    document.getElementById("tempo").value = "";
+    document.getElementById("kmPedalada").value = "";
+    document.getElementById("tempoPedalada").value = "";
+    document.getElementById("passos").value = "";
+}
 
-    // Lógica para exibir gráficos
+// Lógica para exibir gráficos
 function exibirGraficos() {
     // Configurar adaptador de data
     Chart.register(ChartTime);
@@ -140,6 +139,43 @@ function exibirGraficos() {
 
         // Criar gráfico de passos
         criarGrafico("graficoPassos", "Passos", dadosPassos);
+    });
+}
+
+// Função para criar gráfico usando Chart.js
+function criarGrafico(idCanvas, label, dados) {
+    const ctx = document.getElementById(idCanvas).getContext('2d');
+    new Chart(ctx, {
+        type: 'line',
+        data: {
+            datasets: [{
+                label: label,
+                data: dados,
+                borderColor: 'rgba(75, 192, 192, 1)',
+                borderWidth: 1,
+                fill: false
+            }]
+        },
+        options: {
+            scales: {
+                x: {
+                    type: 'time',
+                    time: {
+                        unit: 'day'
+                    },
+                    title: {
+                        display: true,
+                        text: 'Data'
+                    }
+                },
+                y: {
+                    title: {
+                        display: true,
+                        text: label === 'Passos' ? 'Quantidade de Passos' : 'Distância (KM)'
+                    }
+                }
+            }
+        }
     });
 }
 
